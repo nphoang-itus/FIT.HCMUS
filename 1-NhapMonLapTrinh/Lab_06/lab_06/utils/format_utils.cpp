aca99838e822dcb5c4ad04b079af4ee91f1b3f26
nphoang <nphuchoang.itus@gmail.com>
2025-05-11 21:20:05 +0700

    do homework for nmlt/lap06 ver2

--- 1-NhapMonLapTrinh/Lab_06/lab_06/utils/format_utils.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cmath>
-
-using namespace std;
-
-float round_to_decimal(float var, int decimal_places) {
-    float factor = pow(10.0f, decimal_places);
-    return round(var * factor) / factor;
-}
