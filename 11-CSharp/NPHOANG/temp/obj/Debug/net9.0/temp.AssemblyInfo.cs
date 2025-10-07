ef5e5c2e18ae5145d97b67e3c4a0711ab90d9043
nphoang <nphuchoang.itus@gmail.com>
2025-10-07 23:22:24 +0700

    backup

--- 11-CSharp/NPHOANG/temp/obj/Debug/net9.0/temp.AssemblyInfo.cs
+++ 11-CSharp/NPHOANG/temp/obj/Debug/net9.0/temp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("temp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bfd6b5c96d33539d50c002e9b4063ce5188bd3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61034a889ae7193876edb2b76cd2ec69c239f36f")]
 [assembly: System.Reflection.AssemblyProductAttribute("temp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("temp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
